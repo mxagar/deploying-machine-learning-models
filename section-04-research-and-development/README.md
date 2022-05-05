5b8fbfb4c3f7e7c8bef2604e5f5f97af8890a807
Mikel Sagardia <mxagar@gmail.com>
2022-05-05 10:28:32 +0200

    uploading my changes

---
+++ section-04-research-and-development/README.md
@@ -0,0 +1,14 @@
+# Machine Learning Model Deployment Guide: Research and Development Environment
+
+This folder contains a case study in which the [House Prices - Advanced Regression Techniques](https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques/) from Kaggle is prepared in the **research/development environment** . The content structured following the Udemy course [Deployment of Machine Learning Models](https://www.udemy.com/course/deployment-of-machine-learning-models) by Soledad Galli & Christopher Samiullah.
+
+Note that the authors of that course have a repository, which I forked: 
+
+[deploying-machine-learning-models](https://github.com/mxagar/deploying-machine-learning-models)
+
+However, since I started to re-organize and annotate the content substantially, I decided to move the notebooks here manually, to work on them independently. Given that fact, note that notebook and code authorship is originally and mainly from Soledad Galli & Christopher Samiullah.
+
+For a detailed guide of the course, look in `../ML_Deployment_Guide.md`.
+
+Mikel Sagardia, 2022.
+No guarantees.
--- section-04-research-and-development/preprocessors.py
+++ section-04-research-and-development/preprocessors.py
@@ -1,42 +1,52 @@
+from operator import index
 import numpy as np
 import pandas as pd
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
-
-
+# Watch out: we always inherit from BaseEstimator, TransformerMixin
 class TemporalVariableTransformer(BaseEstimator, TransformerMixin):
-	# Temporal elapsed time transformer
+    """Temporal elapsed time transformer."""
 
     def __init__(self, variables, reference_variable):
         
+        # Check that variables is of type list!
         if not isinstance(variables, list):
             raise ValueError('variables should be a list')
         
+        # List of variables we want to computed the year difference for
         self.variables = variables
+        # The reference year variable against which we compute the difference/elapsed time in years
         self.reference_variable = reference_variable
 
     def fit(self, X, y=None):
-        # we need this step to fit the sklearn pipeline
+        # We don't learn anything, BUT
+        # We need this step to fit the sklearn pipeline
         return self
 
     def transform(self, X):
 
-    	# so that we do not over-write the original dataframe
+        # Note that we copy X
+        # So that we do not over-write the original dataframe
         X = X.copy()
         
+        # This is our procedural code, packed in a class
         for feature in self.variables:
             X[feature] = X[self.reference_variable] - X[feature]
 
         return X
 
-
-
-# categorical missing value imputer
+ 
 class Mapper(BaseEstimator, TransformerMixin):
+    """Categorical missing value imputer."""
 
+    # Variables that need the mapping, which mappings
+    # variables: list
+    # mappings: dictionary; however, that is not checked in this code
     def __init__(self, variables, mappings):
 
+        # We check variables is a list
+        # We could check that mappings is a dictionary, too!
         if not isinstance(variables, list):
             raise ValueError('variables should be a list')
 
@@ -44,12 +54,110 @@ class Mapper(BaseEstimator, TransformerMixin):
         self.mappings = mappings
 
     def fit(self, X, y=None):
-        # we need the fit statement to accomodate the sklearn pipeline
+        # We don't learn anything from the data, BUT
+        # We need the fit statement to accommodate the sklearn pipeline
         return self
 
     def transform(self, X):
+        # This is the procedural piece of code, packed in a class!
+        # Note that we copy X to avoid changing the original dataset
         X = X.copy()
         for feature in self.variables:
             X[feature] = X[feature].map(self.mappings)
+
+        return X
+
+class MeanImputer(BaseEstimator, TransformerMixin):
+    """Numerical missing value imputer."""
+
+    def __init__(self, variables):
+        # Check that the variables are of type list
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        # Learn and persist mean values in a dictionary
+        self.imputer_dict_ = X[self.variables].mean().to_dict()
+        return self
+
+    def transform(self, X):
+        # Note that we copy X to avoid changing the original dataset
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature], inplace=True)
+        return X
+
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+    """Groups infrequent categories into a single string"""
+
+    def __init__(self, variables, tol=0.05):
+        # Check that the variables are of type list
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        # Rare if appears less than tol=5%
+        self.tol = tol
+        self.variables = variables
+        self.replace_with = "Rare"
+
+    def fit(self, X, y=None):
+        # Learn persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            # Get the frequencies of each category/level
+            t = pd.Series(X[var].value_counts(normalize=True))
+            # Select frequent labels and save them - or infrequent?
+            #freq_idx = t[t >= (self.tol)].index
+            freq_idx = t[t < (self.tol)].index
+            self.encoder_dict_[var] = list(freq_idx)
+        
+        return self
+
+    def transform(self, X):
+        # Note that we copy X to avoid changing the original dataset
+        X = X.copy()
+        for feature in self.variables:
+            # Replace categories/levels that were detected as rare (fit) with label "Rare"
+            X[feature] = np.where(X[feature].isin(self.encoder_dict_[feature]), X[feature], self.replace_with)
+
+        return X
+
+
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    """String to numbers categorical encoder."""
+
+    def __init__(self, variables):
+        # Check that the variables are of type list
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.variables = variables
+
+    def fit(self, X, y):
+        # We want to order the labels according to their target values
+        # Thus, we add target=y as column
+        temp = pd.concat([X, y], axis=1)
+        temp.columns = list(X.columns) + ["target"]
+
+        # Learn and persist transforming dictionary
+        self.encoder_dict_ = {}
+
+        # Group-By using the variable (ans its categories)
+        # and sort values according to their mean target
+        for var in self.variables:
+            t = temp.groupby([var])["target"].mean().sort_values(ascending=True).index
+            # Save the label-index dictionary for each variable (key=label:value=index)
+            self.encoder_dict_[var] = {k: i for i, k in enumerate(t, 0)}
+
+        return self
+
+    def transform(self, X):
+        # Note that we copy X to avoid changing the original dataset
+        X = X.copy()
+        for feature in self.variables:
+            # Encode labels: map label-index pairs (key=label:value=index)
+            X[feature] = X[feature].map(self.encoder_dict_[feature])
 
         return X
